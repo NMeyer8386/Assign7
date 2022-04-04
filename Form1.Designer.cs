555c6f179ff678784dc9980a2ff544e9dfe3d0fa
Nathaniel Meyer <nathaniel.meyer@lethbridgecollege.ca>
2022-04-04 12:41:03 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace Assign7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.CreatePlayer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GoalsBox = new System.Windows.Forms.TextBox();
+            this.JerseyNumBox = new System.Windows.Forms.TextBox();
+            this.PlayerNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlayerList = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.ReverseSort = new System.Windows.Forms.CheckBox();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.UpdateButton);
+            this.groupBox1.Controls.Add(this.CreatePlayer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.GoalsBox);
+            this.groupBox1.Controls.Add(this.JerseyNumBox);
+            this.groupBox1.Controls.Add(this.PlayerNameBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(391, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 170);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player Information";
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(153, 134);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(94, 29);
+            this.UpdateButton.TabIndex = 4;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // CreatePlayer
+            // 
+            this.CreatePlayer.Location = new System.Drawing.Point(7, 135);
+            this.CreatePlayer.Name = "CreatePlayer";
+            this.CreatePlayer.Size = new System.Drawing.Size(94, 29);
+            this.CreatePlayer.TabIndex = 3;
+            this.CreatePlayer.Text = "Create";
+            this.CreatePlayer.UseVisualStyleBackColor = true;
+            this.CreatePlayer.Click += new System.EventHandler(this.CreatePlayer_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Goals Scored";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Jersey Number";
+            // 
+            // GoalsBox
+            // 
+            this.GoalsBox.Location = new System.Drawing.Point(123, 94);
+            this.GoalsBox.Name = "GoalsBox";
+            this.GoalsBox.Size = new System.Drawing.Size(37, 27);
+            this.GoalsBox.TabIndex = 2;
+            // 
+            // JerseyNumBox
+            // 
+            this.JerseyNumBox.Location = new System.Drawing.Point(123, 60);
+            this.JerseyNumBox.Name = "JerseyNumBox";
+            this.JerseyNumBox.Size = new System.Drawing.Size(37, 27);
+            this.JerseyNumBox.TabIndex = 1;
+            // 
+            // PlayerNameBox
+            // 
+            this.PlayerNameBox.Location = new System.Drawing.Point(123, 26);
+            this.PlayerNameBox.Name = "PlayerNameBox";
+            this.PlayerNameBox.Size = new System.Drawing.Size(125, 27);
+            this.PlayerNameBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player Name";
+            // 
+            // PlayerList
+            // 
+            this.PlayerList.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PlayerList.FormattingEnabled = true;
+            this.PlayerList.ItemHeight = 20;
+            this.PlayerList.Location = new System.Drawing.Point(13, 118);
+            this.PlayerList.Name = "PlayerList";
+            this.PlayerList.Size = new System.Drawing.Size(363, 304);
+            this.PlayerList.TabIndex = 1;
+            this.PlayerList.DoubleClick += new System.EventHandler(this.PlayerList_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(671, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Save,
+            this.Load,
+            this.Exit});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(224, 26);
+            this.Save.Text = "Save Roster";
+            // 
+            // Load
+            // 
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(224, 26);
+            this.Load.Text = "Load Roster";
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(224, 26);
+            this.Exit.Text = "Exit Program";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 58);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Order By";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(275, 26);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Goals";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(137, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Jersey";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Name";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // ReverseSort
+            // 
+            this.ReverseSort.AutoSize = true;
+            this.ReverseSort.Location = new System.Drawing.Point(397, 58);
+            this.ReverseSort.Name = "ReverseSort";
+            this.ReverseSort.Size = new System.Drawing.Size(113, 24);
+            this.ReverseSort.TabIndex = 4;
+            this.ReverseSort.Text = "Reverse Sort";
+            this.ReverseSort.UseVisualStyleBackColor = true;
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(514, 53);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(94, 29);
+            this.SortButton.TabIndex = 5;
+            this.SortButton.Text = "Sort";
+            this.SortButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Player Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(245, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(330, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Goals";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 443);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.ReverseSort);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.PlayerList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button CreatePlayer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox GoalsBox;
+        private System.Windows.Forms.TextBox JerseyNumBox;
+        private System.Windows.Forms.TextBox PlayerNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox PlayerList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem Load;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox ReverseSort;
+        private System.Windows.Forms.Button SortButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Player[] playerArray = new Player[30];  //Array of player objects
+        int counter = 0;                        //Counter to keep track of players
+
+        public class Player
+        {
+            public readonly string readPlayer;
+            public string playerName { get { return readPlayer; } }
+
+            public int jerseyNum { get; }
+
+            public int goals { get; set; }
+
+            public Player(string playerIn, int jerseyIn, int goalsIn)
+            {
+                readPlayer = playerIn;
+                jerseyNum = jerseyIn;
+                goals = goalsIn;
+            }
+        }
+
+        private void CreatePlayer_Click(object sender, EventArgs e)
+        {
+            string player = PlayerNameBox.Text;
+            int jersey, goals;
+            bool success = true;
+
+            if (counter < 30)
+            {
+                while (!int.TryParse(JerseyNumBox.Text, out jersey))    //checks for number in jersey
+                {
+                    MessageBox.Show("Please enter a number.", "Error");
+                    JerseyNumBox.Text = "";
+                    success = false;
+                    break;
+                }
+
+                while (!int.TryParse(GoalsBox.Text, out goals))         //checks for number in goals
+                {
+                    MessageBox.Show("Please enter a number.", "Error");
+                    GoalsBox.Text = "";
+                    success = false;
+                    break;
+                }
+
+                if (success)    //no errors
+                {
+                    playerArray[counter] = new Player(player, jersey, goals);
+                    counter++;
+                    String rowString = stringToRow(player, jersey, goals);
+                    PlayerList.Items.Add(rowString);
+                    PlayerNameBox.Text = "";
+                    JerseyNumBox.Text = "";
+                    GoalsBox.Text = "";
+                }
+            }
+            else   //Max player count error
+            {
+                MessageBox.Show("Maximum Player Count Reached.", "Error");
+                PlayerNameBox.Text = "";
+                JerseyNumBox.Text = "";
+                GoalsBox.Text = "";
+            }
+        }
+
+        public String stringToRow(String name, int jersey, int goals)
+        {
+            String player = name;
+            String jerseyToString = Convert.ToString(jersey);
+            String goalsToString = Convert.ToString(goals);
+
+            String rowString = String.Format("{0, -5}{1, 16}{2, 8}", player, jerseyToString, goalsToString);
+
+            return rowString;
+        }
+
+        private void PlayerList_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
